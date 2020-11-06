38434362380df2ea3980e95e4907c4bc947d99f2
Adrien Guezennec <aguezennec@siliconsalad.com>
2020-11-06 10:59:07 +0100

    :building_construction: add linter and import resolution

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: { '@moodme': './src' },
+        },
+      ],
+    ],
   };
 };
